bdba26f0f050038d27ce9e9db058459c4e95ec68
hong <juheeya401@gmail.com>
2022-10-01 18:01:17 +0900

    create Project jpa-shop

---
+++ src/main/java/com/example/jpashop/JpaShopApplication.java
@@ -0,0 +1,14 @@
+package com.example.jpashop;
+
+import lombok.Getter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaShopApplication.class, args);
+    }
+
+}
