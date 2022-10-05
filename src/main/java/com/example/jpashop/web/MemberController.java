cd722797a7a55db49c2a4ccd096c7c7e44f77f78
aisha <aisha@gccompany.co.kr>
2022-10-05 19:25:44 +0900

    [화면]회원가입

---
+++ src/main/java/com/example/jpashop/web/MemberController.java
@@ -0,0 +1,39 @@
+package com.example.jpashop.web;
+
+import com.example.jpashop.domain.Address;
+import com.example.jpashop.domain.entity.Member;
+import com.example.jpashop.service.MemberService;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @RequestMapping("/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "member/createMemberForm";
+    }
+
+    @PostMapping("/new")
+    public String create(@Valid MemberForm form, BindingResult error) {
+        if (error.hasErrors()) {
+            return "member/createMemberForm";
+        }
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
