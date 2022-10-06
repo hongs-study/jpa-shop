be8f5ce2a9581f72d788d9f3c1f3fb808830bdca
aisha <aisha@gccompany.co.kr>
2022-10-06 10:30:55 +0900

    [화면]상품수정

--- src/main/java/com/example/jpashop/web/ItemController.java
+++ src/main/java/com/example/jpashop/web/ItemController.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,6 +24,21 @@ public class ItemController {
     public String createForm(Model model) {
         model.addAttribute("form", new BookForm());
         return "item/createItemForm";
+    }
+
+    @RequestMapping("/{itemId}/edit")
+    public String updateForm(@PathVariable(name = "itemId") Long itemId, Model model) {
+        Book item = (Book) itemService.findOne(itemId);
+
+        Book form = new Book();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+        model.addAttribute("form", form);
+        return "item/updateItemForm";
     }
 
     @PostMapping("/new")
@@ -42,5 +59,19 @@ public class ItemController {
         List<Item> items = itemService.findItems();
         model.addAttribute("items", items);
         return "item/itemList";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String updateForm(@PathVariable("itemId") Long itemId, @ModelAttribute("form") BookForm form) {
+        // todo 실무에서는 파라미터로 들어오는 itemId 조작해서 들어올 수 있다 => 백엔드에서 요청 사용자의 권한을 체크하는 등의 방법으로 체크하는 것이 기능이 필수적이다
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+        itemService.save(book);
+        return "redirect:/items";
     }
 }
