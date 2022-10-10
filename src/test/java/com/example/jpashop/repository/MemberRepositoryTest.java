93bc1d980e380c3539a735de50521b0719117285
hong <juheeya401@gmail.com>
2022-10-10 18:08:59 +0900

    [순수JPA] SpringDataJpa로 만든 리파지토리(인터페이스)테스트

--- src/test/java/com/example/jpashop/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/jpashop/repository/MemberRepositoryTest.java
@@ -1,7 +1,10 @@
-package com.example.jpashop.domain.entity;
+package com.example.jpashop.repository;
+
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import com.example.jpashop.entity.Member;
-import com.example.jpashop.repository.MemberRepository;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.assertj.core.api.Assertions;
@@ -12,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @SpringBootTest
 class MemberRepositoryTest {
 
@@ -42,4 +46,37 @@ class MemberRepositoryTest {
         Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
     }
 
+    @DisplayName("멤버 CRUD")
+    @Test
+    void crudTest() {
+        //given
+        Member member1 = new Member("회원1");
+        Member member2 = new Member("회원2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // 단건조회
+        Member findMem1 = memberRepository.findById(member1.getId()).get();
+        Member findMem2 = memberRepository.findById(member2.getId()).get();
+        assertThat(member1).isEqualTo(findMem1);
+        assertThat(member2).isEqualTo(findMem2);
+
+        findMem1.setName("변경!!");
+
+        // 다건조회
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        // 건수조회
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        // 삭제
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long afterDelCount = memberRepository.count();
+        assertThat(afterDelCount).isEqualTo(0);
+
+    }
 }
