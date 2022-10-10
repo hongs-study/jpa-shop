afe36ed0c7a989e19dd0fb452909e1b1971efb13
hong <juheeya401@gmail.com>
2022-10-10 17:34:24 +0900

    [순수JPA] 순수JPA로 Member,Team의 CRUD 리파지토리 만들기

--- src/main/java/com/example/jpashop/repository/MemberJpaRepository.java
+++ src/main/java/com/example/jpashop/repository/MemberJpaRepository.java
@@ -1,6 +1,8 @@
 package com.example.jpashop.repository;
 
 import com.example.jpashop.entity.Member;
+import java.util.List;
+import java.util.Optional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
@@ -16,8 +18,19 @@ public class MemberJpaRepository {
         return member;
     }
 
-    public Member find(Long id) {
-        return em.find(Member.class, id);
+    public void delete(Member member) {
+        em.remove(member);
     }
 
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(em.find(Member.class, id));
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m").getResultList();
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class).getSingleResult();
+    }
 }
--- src/test/java/com/example/jpashop/repository/MemberJpaRepositoryTest.java
+++ src/test/java/com/example/jpashop/repository/MemberJpaRepositoryTest.java
@@ -3,6 +3,7 @@ package com.example.jpashop.repository;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import com.example.jpashop.entity.Member;
+import java.util.List;
 import javax.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,7 +32,7 @@ class MemberJpaRepositoryTest {
         //when
         Member savedMember = memberJpaRepository.save(member);
 //        em.clear();
-        Member findMember = memberJpaRepository.find(savedMember.getId());
+        Member findMember = memberJpaRepository.findById(savedMember.getId()).orElseThrow();
 
         //then
         assertThat(member.getId()).isEqualTo(findMember.getId());
@@ -39,4 +40,38 @@ class MemberJpaRepositoryTest {
         assertThat(member).isEqualTo(findMember);
     }
 
+
+    @DisplayName("멤버 CRUD")
+    @Test
+    void crudTest() {
+        //given
+        Member member1 = new Member("회원1");
+        Member member2 = new Member("회원2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        // 단건조회
+        Member findMem1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMem2 = memberJpaRepository.findById(member2.getId()).get();
+        assertThat(member1).isEqualTo(findMem1);
+        assertThat(member2).isEqualTo(findMem2);
+
+        findMem1.setName("변경!!");
+
+        // 다건조회
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        // 건수조회
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        // 삭제
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        long afterDelCount = memberJpaRepository.count();
+        assertThat(afterDelCount).isEqualTo(0);
+
+    }
 }
